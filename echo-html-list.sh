9606d8928e28826527d33d669e76ed04bfa8a3c4
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-02-23 16:03:43 +0100

    Add auto details and summary

--- echo-html-list.sh
+++ echo-html-list.sh
@@ -2,6 +2,7 @@
 
 cd shells
 
+previous_shell=""
 previous_name=""
 sha256sum *.* | while read -r line; do
   file="${line##* }"
@@ -19,13 +20,27 @@ sha256sum *.* | while read -r line; do
   else
     if [ -n "${previous_name}" ]; then
       echo "</ul>"
-      echo
     fi
+
+    shell="${file%%-*}"
+    if [ "${shell}" != "${previous_shell}" ]; then
+      if [ -n "${previous_shell}" ]; then
+        echo "</details>"
+        echo
+      fi
+      echo "<details>"
+      echo "<summary>${shell^}</summary>"
+    fi
+
+    echo
     echo "<h3>${name}</h3>"
     echo "<ul>"
     echo "  <li><a href=\"shells/${sha}\">${file}</a></li>"
   fi
 
+  previous_shell="${shell}"
   previous_name="${name}"
 done
 echo "</ul>"
+echo
+echo "</details>"
